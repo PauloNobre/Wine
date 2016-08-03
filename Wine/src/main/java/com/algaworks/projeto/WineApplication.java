93bd2517a989d363a6e6d0a458eca4c7678e2222
PauloNobre <paulinho140@gmail.com>
2016-08-03 12:11:12 -0300

    adaptações

---
+++ Wine/src/main/java/com/algaworks/projeto/WineApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineApplication.class, args);
+	}
+}
